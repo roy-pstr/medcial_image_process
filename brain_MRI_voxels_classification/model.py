450bb64d1456aec6b67febfa2ad232562424a4f0
roy-pstr <49519972+roy-pstr@users.noreply.github.com>
2020-05-17 11:49:46 +0300

    updated model class and added split to batches

---
+++ brain_MRI_voxels_classification/model.py
@@ -0,0 +1,107 @@
+##################### Brain MRI Voxels Classification Project #####################
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+import random
+
+##################### Sigmoid Activation #####################
+def sigmoid(input):
+    return 1/(1 + np.exp(-input))
+##################### Derivative of a Sigmoid #####################
+def d_sigmoid(out):
+    sig = sigmoid(out)
+    return sig * (1 - sig)
+
+#####################  ReLU Activation #####################
+def relu(input):
+    return np.maximum(input, 0)
+##################### Derivative of a Relu #####################
+def d_relu(d_init, out):
+    d = np.array(d_init, copy = True)
+    d[out < 0] = 0.
+    return d
+
+class model:
+    def __init__(self, input_size, batch_size, hidden_layer_size, output_size, std=1e-4):
+        self.input_size = input_size
+        self.batch_size = batch_size
+        self.hidden_layer_size = hidden_layer_size
+
+        self.params = {}
+        self.params['W1'] = std * np.random.randn(input_size, hidden_layer_size)
+        self.params['b1'] = np.zeros(hidden_layer_size)
+        self.params['W2'] = std * np.random.randn(hidden_layer_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+        self.grads = {}
+        self.grads['W1'] = np.zeros(input_size, hidden_layer_size)
+        self.grads['b1'] = np.zeros(hidden_layer_size)
+        self.grads['W2'] = np.zeros(hidden_layer_size, output_size)
+        self.grads['b2'] = np.zeros(output_size)
+
+        self.z1 = self.h1 = np.zeros(self.batch_size, self.input_size)
+        self.z2 = np.zeros(self.batch_size, 1)
+
+    #####################  Forward Function #####################
+    def forawrd(self, x, y):
+        # X: (NxD), N - batch size, D - vectorized image (1024)
+        # W1: (DxH), H - hidden layer size, b1: (Hx1)
+        # h1: (NxH)
+        # W2: (Hx1), H - hidden layer size, b2: (Nx1)
+        # y_pred: (Nx1)
+
+        # hidden layer:
+        self.z1 = x.dot(self.params['W1']) + self.params['b1']  # fully connected -> (N, H)
+        self.h1 = relu(self.z1)  # ReLU
+
+        # output layer:
+        self.z2 = self.h1.dot(self.params['W2']) + self.params['b2']  # fully connected - > (N, )
+        y_pred = sigmoid(self.z2) # h2
+
+        return y_pred
+
+    #####################  Loss Calculation #####################
+    def calc_loss(self, y_pred, y):
+        loss = np.sqrt(np.sum(np.power((y_pred-y),2))) # should be MSE
+        # accuracy
+        if round(y_pred)==y:
+            accuracy = 1
+        else:
+            accuracy = 0
+        return loss, accuracy
+
+    #####################  Backward Function #####################
+    def backward(self, x, y_train, y_pred, reg = 5e-6):
+        dL_dy = (y_train - y_pred) # [Nx1] ?? -> MSE
+        dy_dz2 = d_sigmoid(y_train) # [Nx1]
+        dL_dz2 = dL_dy * dy_dz2 # [Nx1]
+
+        # W2 gradient (dL_dW2 = dL_dz2 * dz2_dW2)
+        dz2_dW2 = self.h1 # [NxH]
+        dL_dW2 = np.dot(dz2_dW2.transpose(), dL_dz2) # [HxN] * [Nx1] = [Hx1]
+
+        # b2 gradient (dL_db2 = dL_dz2 * dz2_db2 = dL_dz2)
+        dL_db2 = dL_dz2 # [Nx1]
+
+        # W1 gradient (dL_dW1 = dL_dz1 * dz1_dW1)
+        dL_dh1 = np.dot(self.params['W2'], dL_dz2.transpose()) # W2 = dz2_dh1, [Hx1] * [1xN] = [NxH]
+        #dL_dz1 = dL_dh1*dh1_dz1:
+        dL_dz1 = d_relu(dL_dh1, self.z1) # [NxH]
+        dL_dW1 = np.dot(x.transpose(), dL_dz1) # [DxN] * [NxH] = [DxH]
+
+        # b1 gradient (dL_db1 = dL_dz1 * dz1_db1 = dL_dz1)
+        dL_db1 = dL_dz1.sum(axis=0)  # [Hx1]
+
+        # regularization gradient
+        dL_dW1 += reg * 2 * self.params['W1']
+        dL_dW2 += reg * 2 * self.params['W2']
+
+        self.grads = {'W1': dL_dW1, 'b1': dL_db1, 'W2': dL_dW2, 'b2': dL_db2}
+
+    #####################  Parameters Update #####################
+    def update_parameters(self, lr=1e-3):
+        self.params['W1'] -= lr * self.grads['dW1']
+        self.params['b1'] -= lr * self.grads['db1']
+        self.params['W2'] -= lr * self.grads['dW2']
+        self.params['b2'] -= lr * self.grads['db2']
--- brain_MRI_voxels_classification/train.py
+++ brain_MRI_voxels_classification/train.py
@@ -1,27 +1,9 @@
 ##################### Brain MRI Voxels Classification Project #####################
-import numpy as np
-import matplotlib.pyplot as plt
 import os
-import cv2
 import random
-
-##################### Sigmoid Activation #####################
-def sigmoid(input):
-    return 1/(1 + np.exp(-input))
-
-##################### Derivative of a Sigmoid w.r.t. Input #####################
-def d_sigmoid(out):
-    sig = sigmoid(out)
-    return sig * (1 - sig)
-
-#####################  ReLU Activation #####################
-def relu(input):
-    return np.maximum(input, 0)
-
-def d_relu(d_init, out):
-    d = np.array(d_init, copy = True)
-    d[out < 0] = 0.
-    return d
+import cv2
+import model
+import numpy as np
 
 #####################  Dataset Creation #####################
 def create_dataset(train_or_val):
@@ -48,84 +30,37 @@ def create_dataset(train_or_val):
     for features, label in data_arr:
         x.append(features.reshape(-1))
         y.append(label)
-    return [x, y]
+    return np.array(x), np.array(y)
 
-#####################  Forward Function #####################
-def forawrd(X, W1, b1, W2, b2):
-    # X: (NxD), N - batch size, D - vectorized image (1024)
-    # W1: (DxH), H - hidden layer size, b1: (Hx1)
-    # h1: (NxH)
-    # W2: (Hx1), H - hidden layer size, b2: (Nx1)
-    # y_pred: (Nx1)
-
-    # hidden layer:
-    z1 = X.dot(W1) + b1  # fully connected -> (N, H)
-    h1 = relu(z1)  # ReLU
-
-    # output layer:
-    z2 = h1.dot(W2) + b2  # fully connected - > (N, )
-    y_pred = sigmoid(z2)
-
-    return y_pred
-
-#####################  Loss Calculation #####################
-def calc_loss(y_pred, y):
-    loss = np.sqrt(np.sum(np.power((y_pred-y),2)))
-    # accuracy
-    if round(y_pred)==y:
-        accuracy = 1
-    else:
-        accuracy = 0
-    return loss, accuracy
-
-#####################  Backward Function #####################
-def backward(y_train, y_pred):
-    dL_dy = (y_train - y_pred) # [Nx1] ??
-    dy_dz2 = d_sigmoid(y_train) # [Nx1]
-    dL_dz2 = dL_dy * dy_dz2 # [Nx1]
-
-    # W2 gradient (dL_dW2 = dL_dz2 * dz2_dW2)
-    dz2_dW2 = h1 # [NxH]
-    dL_dW2 = np.dot(dz2_dW2.transpose(), dL_dz2) # [HxN] * [Nx1] = [Hx1]
-
-    # b2 gradient (dL_db2 = dL_dz2 * dz2_db2 = dL_dz2)
-    dL_db2 = dL_dz2 # [Nx1]
-
-    # W1 gradient (dL_dW1 = dL_dz1 * dz1_dW1)
-    dL_dh1 = np.dot(W2, dL_dz2.transpose()) # W2 = dz2_dh1, [Hx1] * [1xN] = [NxH]
-    #dL_dz1 = dL_dh1*dh1_dz1:
-    dL_dz1 = d_relu(dL_dh1, z1) # [NxH]
-    dL_dW1 = np.dot(X.transpose(), dL_dz1) # [DxN] * [NxH] = [DxH]
-
-    # b1 gradient (dL_db1 = dL_dz1 * dz1_db1 = dL_dz1)
-    dL_db1 = dL_dz1.sum(axis=0)  # [Hx1]
-
-    # regularization gradient
-    dW1 += reg * 2 * W1
-    dW2 += reg * 2 * W2
-
-    grads = {'W1': dL_dW1, 'b1': dL_db1, 'W2': dL_dW2, 'b2': dL_db2}
-
-    return grads
-
-#####################  Parameters Update #####################
-def update_parameters(grads, lr):
-    W1 -= lr * grads['dW1']
-    b1 -= lr * grads['db1']
-    W2 -= lr * grads['dW2']
-    b2 -= lr * grads['db2']
+#####################  Split to Batches #####################
+def split_to_batches(x,y, batch_size=32):
+    # split [num_of_inputs, N] -> [num_of_inputs / D, D, N]
+    num_of_inputs = x.shape[0]
+    N = x.shape[1]
+    D = batch_size
+    return x.reshape(num_of_inputs / D, D, N), y.reshape(num_of_inputs / D, D, N)
 
 #####################  Main  #####################
 if __name__ == '__main__':
+    num_epochs = 100
 #     x_train: (512, 1024) y_train: (512, )
-    [x_train, y_train], [x_val, y_val] = create_dataset()
-    for epoch in range(num_epochs):
-        for mini_batch in train_batches:
-            #input_batch: (batch_size, 1024)
-            # scores: (batch_size, )
-            y_pred = forward(mini_batch[0])
-            loss, accuracy = calc_loss(y_pred, mini_batch[1])
-            grads = backward() #dW1, db1, dW2, db2
-            update_parameters(grads, lr) #update weights and bais
+    x_train, y_train = create_dataset("training")
+    x_val, y_val = create_dataset("validation")
+    print(type(x_train))
+    print("training data: x_train {}, y_train {}".format(x_train.shape, y_train.shape))
+    print("validation data: x_val {}, y_val {}".format(x_val.shape, y_val.shape))
+    #print("input vector size: ({})".format((x_train[0])))
+    learning_rate = 1e-3
+    batch_size = 32
+    our_model = model(1024, batch_size, 10, 1)
+    # for epoch in range(num_epochs):
+    #     x_train_batches, y_train_batches = split_to_batches(x_train, y_train, batch_size): (512, 1024) -> (16, 32, 1024)
+    #     for x_mini_batch, y_mini_batch in x_train_batches, y_train_batches:
+    #         y_pred = our_model.foward(x_mini_batch, y_mini_batch)
+    #         loss, accuracy = our_model.calc_loss(y_pred, y_mini_batch)
+    #         our_model.backward(x_mini_batch, y_mini_batch, y_pred)
+    #         our_model.update_parameters(learning_rate)
 
-    save_network()
+        ################ print current status #########################
+
+    # save_network()
